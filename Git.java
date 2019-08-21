ec2cc31217772731e8ccf8624d00b77bea1e1c32
Tim Padjen <tpadjen@gmail.com>
2019-08-21 13:52:01 -0600

    Say to everyone

--- Git.java
+++ Git.java
@@ -3,5 +3,7 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("\"git is fun!!!!!!!!!!!\"");
         System.out.println("Hi Mom");
+        System.out.println("Hi Dad");
+        System.out.println("Hi Friend");
     }
 }
